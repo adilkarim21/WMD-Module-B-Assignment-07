6003f9097bf07eddf91f78bb880adfb20a49b98c
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-12-15 16:34:49 +0500

    React Assignment01 Complete

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+
+const rootDiv = document.getElementById("root");
+
+ReactDom.createRoot(rootDiv).render(
+  <div>
+    <h1>Hello React!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus rerum incidunt minus qui rem, tempore assumenda. Enim explicabo aliquam porro? Minus labore quia repudiandae, ducimus veritatis nihil! Quas, deleniti reprehenderit? Temporibus maxime officia, et voluptates quos odit vitae maiores esse illum ab assumenda iusto provident iste sapiente dolore sit adipisci veritatis molestiae error ut commodi unde veniam facere ad. Laboriosam reprehenderit error a et, nihil suscipit eius placeat dolor pariatur iusto vitae quisquam odio possimus consequatur. Sequi delectus nemo quia eos repellat incidunt provident necessitatibus odio quam, ad magnam enim doloribus veniam maiores molestiae, iste officia, quo tempore error voluptatum?</p>
+    <img src="/public/vite.svg" alt="" />
+    <img src="/src/assets/react.svg" alt="" />
+  </div>
+);
